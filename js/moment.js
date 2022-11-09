0b22786e99fc97d9cb0f7a53a67464c586fc38a6
furstamp <gyket.pentest.133971@protonmail.com>
2022-11-08 23:47:29 -0300

    estrutura melhorada do moment.js

--- js/moment.js
+++ js/moment.js
@@ -3,7 +3,10 @@
 ;(function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) : global.moment = factory()
-}(this, (function () { 'use strict';
+}
+
+(this, (function () {
+    'use strict';
 
     var hookCallback;
 
